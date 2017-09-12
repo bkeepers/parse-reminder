6ed78daee1e756011f04569e441fd8ed8b306062
Brandon Keepers <bkeepers@github.com>
2017-09-11 18:46:54 -0700

    A few explanatory coments

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ const matcher = /^remind (me)(?: to )?(.*)$/
 module.exports = (input, from) => {
   const match = input.match(matcher)
   if (!match) {
+    // This doesn't look like a reminder, so bail early
     return null
   }
 
+  // Pull out the initial matches
   let [, who, what] = match
 
+  // Use chrono to extract the `when` from the `what`
   const when = chrono.parse(what, from, {forwardDate: true})
 
+  // Remove any time expressions from the `what`
   when.forEach(w => {
     what = what.replace(w.text, '')
   })
 
+  // Clean up whitespace and common connecting words
   what = what.trim()
   what = what.replace(/^(to|that) /, '').replace(/ on$/, '')
 
